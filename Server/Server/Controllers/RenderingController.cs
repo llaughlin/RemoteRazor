c411533726af12a43e2159c209c05551abcdcc3d
Logan Laughlin <loganlaughlin@gmail.com>
2015-07-31 13:50:33 -0500

    Initial commit

---
+++ Server/Server/Controllers/RenderingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Server.Controllers
+{
+    public class RenderingController : Controller
+    {
+        // GET: Rendering
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Server/Server/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Server.Startup))]
+namespace Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
